fa23631c9f97374f1cbf2633cec5e73228e3aa8c
Ben Hong <bencodezen@gmail.com>
2019-01-09 22:07:23 -0500

    feature: Add ability to remove future generated posts from build

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+const fs = require('fs')
+
 module.exports = (options, ctx) => {
     return {
         name: 'vuepress-plugin-janitor',
 
-        // Purpose is to look over the build files and run them through some sort of filter that will modify the output in some fashion.
-        async generated (pagePaths) {
-            // First thing I try to do with anything is make sure something is working before diving too deep in the rabbit hole
-            console.log(pagePaths)
+        generated () {
+            ctx.pages.map(page => {
+                const currentDateUTC = new Date().toUTCString()
+                const currentDateISO = new Date(currentDateUTC).toISOString()
+
+                if (page.frontmatter.date) {
+                    const isFuturePost = page.frontmatter.date > currentDateISO
+
+                    if (isFuturePost) {
+                        fs.unlinkSync(ctx.outDir + page.path)
+
+                        console.log(`♻️ Future Post: ${page.frontmatter.title} was removed from the build directory.`)
+                    }
+                }
+            })
         }
     }
 }
